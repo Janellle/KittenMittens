f0387dcb59b8b69a5317ff7304a63b7bb2726946
EC2 Default User <ec2-user@ip-172-31-21-72.us-west-2.compute.internal>
2020-03-23 23:19:37 +0000

    Homepage

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express();
 //const session = require('express-session');
 
 app.set("view engine", "ejs");
+app.engine('html', require('ejs').renderFile);
 app.use(express.static("public")); //folder for images, css, js
 app.use(express.urlencoded()); //use to parse data sent using the POST method
 
 app.get("/", async function(req, res){
-    res.render("index");
+    res.render("index.html");
 }); //root
 
 
